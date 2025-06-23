90b4836a4cb104584b7e4e9e89f0645ddc196659
Sarah <sarah_portella@hotmail.com>
2025-06-23 00:10:53 -0300

    aaaaaaaaaaaaaaaaaaaaa

--- Trabalho_BD2/pgadmin/consultas/consulta_3.sql
+++ Trabalho_BD2/pgadmin/consultas/consulta_3.sql
@@ -1,20 +1,20 @@
 -- Função para listar produtos com filtros opcionais
 -- Esta função permite filtrar produtos por categoria, restrições alimentares e se são bebidas alco
 CREATE OR REPLACE FUNCTION listar_produtos_com_filtros(
-    p_categorias TEXT[] DEFAULT NULL,         -- Uma ou mais categorias
-    p_sem_gluten BOOLEAN DEFAULT NULL,        -- TRUE para sem glúten
-    p_sem_lactose BOOLEAN DEFAULT NULL,       -- TRUE para sem lactose
-    p_e_alcoolico BOOLEAN DEFAULT NULL        -- TRUE para bebidas alcoólicas, FALSE para não alcoólicas
+    p_categorias TEXT[] DEFAULT NULL,
+    p_sem_gluten BOOLEAN DEFAULT NULL,
+    p_sem_lactose BOOLEAN DEFAULT NULL,
+    p_e_alcoolico BOOLEAN DEFAULT NULL
 )
 RETURNS TABLE (
-    nome_produto TEXT,
-    categoria TEXT,
-    restricao TEXT,
+    nome_produto VARCHAR,
+    categoria VARCHAR,
+    restricao VARCHAR,
     tipo_produto TEXT,
     e_alcoolico BOOLEAN,
-    preco NUMERIC,
-    peso NUMERIC,
-    unidade TEXT
+    preco DOUBLE PRECISION,
+    peso DOUBLE PRECISION,
+    unidade VARCHAR
 ) AS $$
 BEGIN
     RETURN QUERY
@@ -29,23 +29,19 @@ BEGIN
             WHEN p.Acompanhamento THEN 'Acompanhamento'
             ELSE 'Outro'
         END AS tipo_produto,
-        b.E_Alcoolico,
-        p.Preco_prod,
-        p.Peso_prod,
+        CASE 
+            WHEN p.Bebida THEN b.E_Alcoolico 
+            ELSE NULL 
+        END AS e_alcoolico,
+        p.Preco_prod::DOUBLE PRECISION,  --  Cast obrigatório
+        p.Peso_prod::DOUBLE PRECISION,   -- Cast obrigatório
         p.Unidade_medida
     FROM Produto p
     LEFT JOIN Bebida b ON p.Indice_prod = b.Indice_prod
     WHERE 
-        --  Filtro por categoria
         (p_categorias IS NULL OR p.Categoria = ANY(p_categorias))
-        
-        -- Filtro por restrição: sem glúten
         AND (p_sem_gluten IS NULL OR (p_sem_gluten = TRUE AND p.Restricao_alimentar ILIKE '%sem glúten%'))
-
-        -- Filtro por restrição: sem lactose
         AND (p_sem_lactose IS NULL OR (p_sem_lactose = TRUE AND p.Restricao_alimentar ILIKE '%sem lactose%'))
-
-        -- Filtro por álcool (aplica só para bebidas)
         AND (
             p.Bebida = FALSE 
             OR p_e_alcoolico IS NULL 
@@ -54,6 +50,10 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
+
 -- Exemplo de uso da função com lnches da categoria 'Bovino' e bebidas alcoólicas:
-SELECT * FROM listar_produtos_com_filtros(ARRAY['Bovino'], NULL, NULL, TRUE);
+SELECT * FROM listar_produtos_com_filtros(NULL, NULL, NULL, TRUE);
+
+SELECT * FROM listar_produtos_com_filtros(ARRAY['Bovino','Suíno'], NULL, NULL, TRUE);
 --aaaa cmit

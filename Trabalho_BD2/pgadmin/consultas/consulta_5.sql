753a84089c6ed02fb202c8c9d93c7746a179d447
Rafa-Cami <caminagarafaella@gmail.com>
2025-06-23 02:26:33 -0300

    fim consultas 22-06

--- Trabalho_BD2/pgadmin/consultas/consulta_5.sql
+++ Trabalho_BD2/pgadmin/consultas/consulta_5.sql
@@ -4,10 +4,10 @@ CREATE OR REPLACE FUNCTION produtos_mais_vendidos_periodo(
     p_data_fim DATE
 )
 RETURNS TABLE (
-    id_produto INT,
+    id_produto INTEGER,
     nome_produto TEXT,
     tipo_produto TEXT,
-    quantidade_vendida INT
+    quantidade_vendida INTEGER
 ) AS $$
 BEGIN
     RETURN QUERY
@@ -19,8 +19,8 @@ BEGIN
     )
     
     SELECT 
-        pr.Indice_prod AS id_produto,
-        pr.Nome_prod,
+        pr.Indice_prod::INTEGER AS id_produto,
+        pr.Nome_prod::TEXT AS nome_produto,  -- << Cast aqui
         CASE 
             WHEN pr.Lanche THEN 'Lanche'
             WHEN pr.Bebida THEN 'Bebida'
@@ -28,7 +28,7 @@ BEGIN
             WHEN pr.Acompanhamento THEN 'Acompanhamento'
             ELSE 'Outro'
         END AS tipo_produto,
-        COUNT(*) AS quantidade_vendida
+        COUNT(*)::INTEGER AS quantidade_vendida
     FROM produtos_pedidos pp
     JOIN Produto pr ON pr.Indice_prod = pp.Indice_prod
     GROUP BY pr.Indice_prod, pr.Nome_prod, pr.Lanche, pr.Bebida, pr.Sobremesa, pr.Acompanhamento
@@ -36,6 +36,49 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
 --Exemplos de uso:
 
 SELECT * FROM produtos_mais_vendidos_periodo('2024-06-01', '2024-06-10');
+
+-- -- Função para retornar os produtos mais vendidos em um período específico
+-- CREATE OR REPLACE FUNCTION produtos_mais_vendidos_periodo(
+--     p_data_inicio DATE,
+--     p_data_fim DATE
+-- )
+-- RETURNS TABLE (
+--     id_produto INT,
+--     nome_produto TEXT,
+--     tipo_produto TEXT,
+--     quantidade_vendida INT
+-- ) AS $$
+-- BEGIN
+--     RETURN QUERY
+--     WITH produtos_pedidos AS (
+--         SELECT pe.Indice_prod, p.Id_pedido
+--         FROM Ped_Escolhe_Prod pe
+--         JOIN Pedido p ON pe.Id_pedido = p.Id_pedido  
+--         WHERE p.Data_pedido BETWEEN p_data_inicio AND p_data_fim
+--     )
+    
+--     SELECT 
+--         pr.Indice_prod AS id_produto,
+--         pr.Nome_prod,
+--         CASE 
+--             WHEN pr.Lanche THEN 'Lanche'
+--             WHEN pr.Bebida THEN 'Bebida'
+--             WHEN pr.Sobremesa THEN 'Sobremesa'
+--             WHEN pr.Acompanhamento THEN 'Acompanhamento'
+--             ELSE 'Outro'
+--         END AS tipo_produto,
+--         COUNT(*) AS quantidade_vendida
+--     FROM produtos_pedidos pp
+--     JOIN Produto pr ON pr.Indice_prod = pp.Indice_prod
+--     GROUP BY pr.Indice_prod, pr.Nome_prod, pr.Lanche, pr.Bebida, pr.Sobremesa, pr.Acompanhamento
+--     ORDER BY quantidade_vendida DESC;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+-- --Exemplos de uso:
+
+-- SELECT * FROM produtos_mais_vendidos_periodo('2024-06-01', '2024-06-10');
